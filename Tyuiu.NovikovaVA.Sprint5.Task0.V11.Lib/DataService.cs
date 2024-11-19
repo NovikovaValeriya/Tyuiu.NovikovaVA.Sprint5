19132f26cefb7bfe1c167d8bd402427770e66a9f
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-11-19 18:04:55 +0500

    Add project files.

---
+++ Tyuiu.NovikovaVA.Sprint5.Task0.V11.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.NovikovaVA.Sprint5.Task0.V11.Lib
+{
+    public class DataService : ISprint5Task0V11
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            double y = (4 - Math.Pow(x, 3)) / (Math.Pow(x, 2));
+            y = Math.Round(y, 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
+
---
+++ Tyuiu.NovikovaVA.Sprint5.Task0.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.NovikovaVA.Sprint5.Task0.V11.Lib;
+namespace Tyuiu.NovikovaVA.Sprint5.Task0.V11.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckExistsFile()
+        {
+            
+            string path = @"C:\Users\User\source\repos\Tyuiu.NovikovaVA.Sprint5.Task0.V11\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(fileExists, wait);
+        }
+    }
+}
